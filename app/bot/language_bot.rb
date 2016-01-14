37e62f5859c7f8c3b5351a30b6b8cc908d3cdda9
An Tran Minh <tranminhan.my@gmail.com>
2016-01-14 20:12:37 +0700

    command to set locale

--- app/bot/language_bot.rb
+++ app/bot/language_bot.rb
@@ -5,10 +5,24 @@ class LanguageBot < SlackRubyBot::Bot
     user = User.find_or_create_by(username: data.user) do |u|
       u.locale = :en
     end
-    puts data
-    puts user.locale
+    
     I18n.locale = user.locale
     text = I18n.t "hello", user: user.username
     client.say(text: text, channel: data.channel)
+  end
+
+  command 'locale' do |client, data, match|
+    user = User.find_or_create_by(username: data.user)
+    # 
+    if I18n.available_locales.map(&:to_s).include? match[:expression]
+      user.locale = match[:expression]
+      user.save
+
+      text = "Set locale successfully"
+      client.say(text: text, channel: data.channel)
+    else 
+      text = "Incorrect locale"
+      client.say(text: text, channel: data.channel)
+    end 
   end
 end
