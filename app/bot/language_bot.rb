e16b9e9ac9588456b55cd3a0f155ecc556cba776
An Tran Minh <tranminhan.my@gmail.com>
2016-01-14 19:57:55 +0700

    user locale for bot

--- app/bot/language_bot.rb
+++ app/bot/language_bot.rb
@@ -1,3 +1,14 @@
 class LanguageBot < SlackRubyBot::Bot
   # leave it empty for now
+  command 'hello' do |client, data, match|
+    # client.say(text: t("hello <#{data.user}>!", channel: data.channel)
+    user = User.find_or_create_by(username: data.user) do |u|
+      u.locale = :en
+    end
+    puts data
+    puts user.locale
+    I18n.locale = user.locale
+    text = I18n.t "hello", user: user.username
+    client.say(text: text, channel: data.channel)
+  end
 end
